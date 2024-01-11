269b4c2b0a6800dc269b6336930204f72fcee753
Snehal <snehal.cs21@bmsce.ac.in>
2024-01-11 16:48:41 +0530

    task

---
+++ src/Components/main.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./main.css"
+export default function Main(){
+    return(
+        <div>
+            <div className = "container2">
+                <div className = "one"></div>
+                <div className = "two"></div>
+                <div className = "third"></div>
+            </div>
+        </div>
+    )
+}
